72c764f307101e4e219df29f063fde1228eb24c5
Tariq <tariqahmed1912@gmail.com>
2020-05-03 15:59:09 +0530

    Power function

--- calc.py
+++ calc.py
@@ -13,11 +13,12 @@ print("1.Add")
 print("2.Subtract")
 print("3.Multiply")
 print("4.Divide")
+print("5.Power")
 
 while True:
-    choice = input("Enter choice(1/2/3/4): ")
+    choice = input("Enter choice(1/2/3/4/5): ")
 
-    if choice in ('1', '2', '3', '4'):
+    if choice in ('1', '2', '3', '4','5'):
         num1 = float(input("Enter first number: "))
         num2 = float(input("Enter second number: "))
 
@@ -29,6 +30,9 @@ while True:
         
         elif choice=='3':
             print(mul(num1,num2))
-        break
+        
+	elif choice == '5':
+	    print(pow(num1, num2))
+	break
     else:
         print("Invalid Input")
